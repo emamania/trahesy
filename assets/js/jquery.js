d48832943970a95a1f4fe933ee00a07b6e76e8ee
emamania <centinela1526@gmail.com>
2019-12-16 07:06:37 -0500

    first commit

---
+++ assets/js/jquery.js
@@ -0,0 +1,77 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Set rates + misc */
+var taxRate = 0.05;
+var shippingRate = 15.00; 
+var fadeTime = 300;
+
+
+document.getElementById('nroquantity').addEventListener('change', ()=>{
+});
+
+var nro = document.getElementById('nroquantity').value;
+var price = document.getElementsByClassName('price');
+console.log( price);
+
+/* nro.addEventListener('change', (event) =>{
+    const resultaa = document.getElementById('logg');
+    console.log('result'.resultaa);    
+}); */
+
+/* Assign actions */
+/* const assign = document.querySelector('.number-input input');
+const logg = document.getElementById('log');
+console.log(assign);
+console.log(logg);
+
+
+
+assign.addEventListener('change', function() {    
+    //logg.textContent = e.target.value;
+}); */
+
+/* function updateValue(e) {
+  logg.textContent = e.target.value;
+} */
